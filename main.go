a274e452154be4d8b75ac2f887871c071020af19
Adam Olech <nddr89@gmail.com>
2022-06-12 16:14:32 +0200

    main: get load configuration

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"go.uber.org/zap"
+
+	"github.com/Zamerykanizowana/replicated-file-system/config"
 	"github.com/Zamerykanizowana/replicated-file-system/logging"
 )
 
+var appConfig config.Config
+
 func main() {
 	logging.Configure()
+	appConfig = config.ReadConfig()
+
+	zap.L().Info("Initializing FS", zap.String("local_path", appConfig.LocalDir))
 }
