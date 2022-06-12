f114718ec609f7cb3040e12b19d993dfcd3c92e0
Adam Olech <nddr89@gmail.com>
2022-06-12 16:20:10 +0200

    main: log commit and branch

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 
 var appConfig config.Config
 
+var (
+	branch = "unknown-branch"
+	commit = "unknown-commit"
+)
+
 func main() {
 	logging.Configure()
 	appConfig = config.ReadConfig()
 
+	zap.L().Info("Hello!", zap.String("commit", commit), zap.String("branch", branch))
 	zap.L().Info("Initializing FS", zap.String("local_path", appConfig.LocalDir))
 }
