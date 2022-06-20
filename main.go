4e79ddedb630fd424fb462b32aff74a367637d6a
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2022-06-20 19:58:59 +0200

    remove test log from main

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"errors"
-
-	"go.uber.org/zap"
-
 	"github.com/Zamerykanizowana/replicated-file-system/logging"
 )
 
 func main() {
 	logging.Configure()
-	zap.L().With(zap.Error(errors.New("lkol")), zap.Int("dupa", 1)).Info("o matko")
 }
