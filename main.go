66455a913d8e7eb6ee3e88d0044a1997c78ce4da
Adam Olech <nddr89@gmail.com>
2022-06-22 02:24:00 +0200

    Create rfs package

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"syscall"
-	"time"
 
 	"go.uber.org/zap"
 
-	"github.com/hanwen/go-fuse/v2/fs"
-
 	"github.com/Zamerykanizowana/replicated-file-system/config"
 	"github.com/Zamerykanizowana/replicated-file-system/logging"
+	"github.com/Zamerykanizowana/replicated-file-system/rfs"
 )
 
 var appConfig config.Config
@@ -22,27 +17,6 @@ var (
 	commit = "unknown-commit"
 )
 
-type rfsRoot struct {
-	fs.LoopbackNode
-}
-
-func newRfsRoot(r *fs.LoopbackRoot, p *fs.Inode, n string, st *syscall.Stat_t) fs.InodeEmbedder {
-	node := &rfsRoot{
-		LoopbackNode: fs.LoopbackNode{
-			RootData: r,
-		},
-	}
-	return node
-}
-
-func (n *rfsRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
-	fh, flags, _ := n.LoopbackNode.Open(ctx, flags)
-
-	fakeError := syscall.Errno(syscall.EBUSY)
-
-	return fh, flags, fakeError
-}
-
 func main() {
 	logging.Configure()
 	appConfig = config.ReadConfig()
@@ -50,21 +24,9 @@ func main() {
 	zap.L().Info("Hello!", zap.String("commit", commit), zap.String("branch", branch))
 	zap.L().Info("Initializing FS", zap.String("local_path", appConfig.Paths.FuseDir))
 
-	if err := os.MkdirAll(appConfig.Paths.FuseDir, 0777); err != nil {
-		zap.L().Fatal("unable to create local directory", zap.Error(err))
-	}
+	server := rfs.NewRfsFuseServer(appConfig)
 
-	root := &fs.LoopbackRoot{
-		NewNode: newRfsRoot,
-		Path:    appConfig.Paths.MirrorDir,
-	}
-
-	sec := time.Second
-
-	server, err := fs.Mount(appConfig.Paths.FuseDir, newRfsRoot(root, nil, "", nil), &fs.Options{
-		AttrTimeout:  &sec,
-		EntryTimeout: &sec,
-	})
+	err := server.Mount()
 
 	if err != nil {
 		zap.L().Fatal("unable to mount fuse filesystem", zap.Error(err))
@@ -73,5 +35,5 @@ func main() {
 	zap.L().Info("unmount by calling", zap.String("cmd", fmt.Sprintf("fusermount -u %s", appConfig.Paths.FuseDir)))
 
 	// Wait until user unmounts FS
-	server.Wait()
+	server.Server.Wait()
 }
---
+++ rfs/fuse.go
@@ -0,0 +1,55 @@
+package rfs
+
+import (
+	"os"
+	"time"
+
+	"github.com/Zamerykanizowana/replicated-file-system/config"
+	"github.com/Zamerykanizowana/replicated-file-system/logging"
+	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+	"go.uber.org/zap"
+)
+
+type RfsFuseServer struct {
+	LoopbackRoot *fs.LoopbackRoot
+	RfsRoot      fs.InodeEmbedder
+	Config       config.Config
+	Server       *fuse.Server
+}
+
+func (r *RfsFuseServer) Mount() error {
+	sec := time.Second
+
+	s, err := fs.Mount(r.Config.Paths.FuseDir, newRfsRoot(r.LoopbackRoot, nil, "", nil), &fs.Options{
+		AttrTimeout:  &sec,
+		EntryTimeout: &sec,
+	})
+
+	if err == nil {
+		r.Server = s
+	}
+
+	return err
+}
+
+func NewRfsFuseServer(c config.Config) *RfsFuseServer {
+	if err := os.MkdirAll(c.Paths.FuseDir, 0777); err != nil {
+		zap.L().Fatal("unable to create local directory", zap.Error(err))
+	}
+
+	root := &fs.LoopbackRoot{
+		NewNode: newRfsRoot,
+		Path:    c.Paths.MirrorDir,
+	}
+
+	return &RfsFuseServer{
+		LoopbackRoot: root,
+		RfsRoot:      newRfsRoot(root, nil, "", nil),
+		Config:       c,
+	}
+}
+
+func init() {
+	logging.Configure()
+}
