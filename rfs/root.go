9b5ebb64ed79498f5be6b2588b6575d361373c4a
Adam Olech <nddr89@gmail.com>
2022-06-25 14:01:59 +0200

    rfs/root: return EBUSY directly

--- rfs/root.go
+++ rfs/root.go
@@ -23,7 +23,7 @@ func newRfsRoot(r *fs.LoopbackRoot, p *fs.Inode, n string, st *syscall.Stat_t) f
 func (n *rfsRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
 	fh, flags, _ := n.LoopbackNode.Open(ctx, flags)
 
-	fakeError := syscall.Errno(syscall.EBUSY)
+	fakeError := syscall.EBUSY
 
 	return fh, flags, fakeError
 }
