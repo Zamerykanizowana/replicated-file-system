e8a6bf15f5a970d5b2875e8f85370666af4a0145
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2022-07-25 22:02:54 +0200

    Remove open func

--- rfs/root.go
+++ rfs/root.go
@@ -97,11 +97,3 @@ func (n *rfsRoot) CopyFileRange(ctx context.Context, fhIn fs.FileHandle,
 
 	return fflags, fakeError
 }
-
-func (n *rfsRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
-	fh, flags, _ := n.LoopbackNode.Open(ctx, flags)
-
-	fakeError := syscall.EBUSY
-
-	return fh, flags, fakeError
-}
