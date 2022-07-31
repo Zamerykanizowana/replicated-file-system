f42a581979d53ae5a46083a8cc8cb8bb9de6f312
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2022-07-31 18:26:14 +0200

    Fix temporary Read

--- rfs/root_file.go
+++ rfs/root_file.go
@@ -11,11 +11,25 @@ import (
 
 func NewRfsFile(fd int) fs.FileHandle {
 	log.Info().Int("fd: ", fd).Msg("From NewRfsFile: ")
-	return &rfsFile{fs.NewLoopbackFile(fd)}
+	return &rfsFile{fs.NewLoopbackFile(fd).(loopbackFile)}
+}
+
+type loopbackFile interface {
+	Release(ctx context.Context) syscall.Errno
+	Getattr(ctx context.Context, out *fuse.AttrOut) syscall.Errno
+	Write(ctx context.Context, data []byte, off int64) (written uint32, errno syscall.Errno)
+	Getlk(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32, out *fuse.FileLock) syscall.Errno
+	Setlk(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) syscall.Errno
+	Setlkw(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) syscall.Errno
+	Lseek(ctx context.Context, off uint64, whence uint32) (uint64, syscall.Errno)
+	Flush(ctx context.Context) syscall.Errno
+	Fsync(ctx context.Context, flags uint32) syscall.Errno
+	Setattr(ctx context.Context, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno
+	Allocate(ctx context.Context, off uint64, size uint64, mode uint32) syscall.Errno
 }
 
 type rfsFile struct {
-	fs.FileHandle
+	loopbackFile
 }
 
 func (f *rfsFile) Read(ctx context.Context, buf []byte, off int64) (res fuse.ReadResult, errno syscall.Errno) {
